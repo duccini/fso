608255084f5ecc6e56739c9fc6d53076e1b8d92e
Guilherme Duccini <guilhermeduccini@gmail.com>
2024-04-12 22:06:01 -0300

    Part 3: Section A

--- Readme.md
+++ Readme.md
@@ -1,6 +1,8 @@
-# University of Helsinki - FullStack Program [link](https://fullstackopen.com/en/)
+# University of Helsinki - FullStack Program
 
 ### This course serves as an introduction to modern web application development with JavaScript. The main focus is on building single page applications with ReactJS that use REST APIs built with Node.js. The course also contains a section on GraphQL, a modern alternative to REST APIs.
+
+[link](https://fullstackopen.com/en/)
 
 ## Course contents
 
--- part3/app1/index.js
+++ part3/app1/index.js
@@ -53,11 +53,25 @@ app.delete("/api/notes/:id", (request, response) => {
   }
 });
 
-app.post("/api/notes", (request, response) => {
+const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
 
-  const note = request.body;
-  note.id = maxId + 1;
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
 
   notes = notes.concat(note);
 
